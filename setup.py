3cd50b4d33261471498bf186864ee81eca9997c8
jamesf-brainlabs <james.f@brainlabsdigital.com>
2019-10-09 17:31:32 +0100

    Update version

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup
 setup(
     name="Brainlabs YAML Generator",
     packages=["Brainlabs-YAML-Generator"],
-    version="1",
+    version="2",
     description="Tool for generating brainlabs.yaml files.",
     author="James Freeman",
     author_email="james.f@brainlabsdigital.com",
     url="https://github.com/Brainlabs-Digital/Brainlabs-YAML-Generator",
-    # entry_points={"console_scripts": ["bl-yaml=Brainlabs-YAML-Generator.main:main"]},
+    scripts=["Brainlabs-YAML-Generator/bl-yaml"],
 )

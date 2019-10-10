69f6cc296e7efea88206ba296b27e62b4f407401
jamesf-brainlabs <james.f@brainlabsdigital.com>
2019-10-10 10:28:19 +0100

    Improve packaging versioning

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup
 setup(
     name="blyaml",
     packages=["blyaml"],
-    version="1",
+    version="2",
     description="Tool for generating brainlabs.yaml files.",
     author="James Freeman",
     author_email="james.f@brainlabsdigital.com",
     url="https://github.com/Brainlabs-Digital/Brainlabs-YAML-Generator",
-    install_requires=["pyyaml", "pyinquirer"],
+    install_requires=["pyyaml>=5.1.1", "pyinquirer"],
     entry_points={"console_scripts": ["blyaml = blyaml.main:main"]},
 )

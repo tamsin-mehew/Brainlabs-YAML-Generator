20c0d3b6fe03902b076bdbe7a94c6c547279ed70
jamesf-brainlabs <james.f@brainlabsdigital.com>
2019-10-14 14:59:51 +0100

    Fixes version update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="blyaml",
     packages=["blyaml"],
-    version="18",
+    version="19",
     description="Tool for generating brainlabs.yaml files.",
     author="James Freeman",
     author_email="james.f@brainlabsdigital.com",

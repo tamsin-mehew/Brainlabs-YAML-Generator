1022c1b68858bc186b98be9cc5bb5f2560de3830
jamesf-brainlabs <james.f@brainlabsdigital.com>
2019-10-17 12:46:49 +0100

    Implement test

--- tests/validators_test.py
+++ tests/validators_test.py
@@ -1,21 +1,33 @@
 from pytest import raises
 from PyInquirer import ValidationError
+from prompt_toolkit.document import Document
 
 import blyaml.validators as validators
 
 
 def test_valid_email_prefix() -> None:
     email_validator = validators.ValidEmailPrefix()
-    email_validator.validate("james")
-    email_validator.validate("james.f")
+    assert email_validator.validate(Document("james"))
+    assert email_validator.validate(Document("james.f"))
     with raises(ValidationError):
-        email_validator.validate("james@brainlabsdigital.com")
+        email_validator.validate(Document("james@brainlabsdigital.com"))
     with raises(ValidationError):
-        email_validator.validate("james f")
+        email_validator.validate(Document("james f"))
 
 
 def test_valid_email_prefix_list() -> None:
-    pass
+    email_list_validator = validators.ValidEmailPrefixList()
+    assert email_list_validator.validate(Document("james"))
+    assert email_list_validator.validate(Document("james.f"))
+    assert email_list_validator.validate(Document("james, sam"))
+    assert email_list_validator.validate(Document("james.f, sam.d"))
+    assert email_list_validator.validate(Document("james,sam.d"))
+    assert email_list_validator.validate(Document("james,sam,marie"))
+    assert email_list_validator.validate(Document("james,  sam,  marie"))
+    with raises(ValidationError):
+        email_list_validator.validate(Document("james f, sam d,  marie"))
+    with raises(ValidationError):
+        email_list_validator.validate(Document("james f"))
 
 
 def test_valid_client_id() -> None:
@@ -35,7 +47,18 @@ def test_valid_url_list() -> None:
 
 
 def test_valid_cron() -> None:
-    pass
+    cron_validator = validators.ValidCron()
+    assert cron_validator.validate(Document("* * * * *"))
+    assert cron_validator.validate(Document("7 7 7 7 6"))
+    assert cron_validator.validate(Document("7 7 * * *"))
+    assert cron_validator.validate(Document("07 07 * * *"))
+    assert cron_validator.validate(Document("07 07 1 1 1"))
+    with raises(ValidationError):
+        cron_validator.validate(Document("Hello World!"))
+    with raises(ValidationError):
+        cron_validator.validate(Document("1 1 1 1 1 1"))
+    with raises(ValidationError):
+        cron_validator.validate(Document("* * * * * *"))
 
 
 def test_valid_directory() -> None:
